6b309384b47b5ee0d0c66eee5293bd0bfb86aaa3
Pandelo13 <81708768+TomasBarrigon@users.noreply.github.com>
2023-04-12 15:31:10 -0300

    Delete RutaDefault.ts

--- RutaDefault.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-export const RutaDefault = Router();
-
-RutaDefault.get('/', (_req, _res) => {
-  _res.send("Bienvenido a mi api para chat! Chat (Usuario, Mensaje) (MongoDB)");
-});
